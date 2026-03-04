e8fcdd770172e41dda14083910e11a0f2bb2f9fe
Iqbal <iqbalhossain6615@gmail.com>
2026-03-04 16:40:10 +0600

    displaying details using modal partially done

--- script/index.js
+++ script/index.js
@@ -22,6 +22,41 @@ const loadLevelWord = (id) => {
     
 };
 
+const loadWordDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+    
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetails(details.data);
+    
+};
+
+const displayWordDetails = (word) => {
+    console.log(word.data);
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `
+    <div class="">
+                    <h2 class="tex-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>     :${word.pronunciation})</h2>
+                </div>
+                <div class="">
+                    <h2 class="font-bold">Meaning</h2>
+                    <p>${word.meaning}</p>
+                </div>
+                <div class="">
+                    <h2 class="font-bold">Example</h2>
+                    <p>${word.sentence}</p>
+                </div>
+                <div class="">
+                    <h2 class="font-bold">Synonym</h2>
+                    <span class="btn">syn1</span>
+                    <span class="btn">syn1</span>
+                    <span class="btn">syn1</span>
+                </div>`;
+    document.getElementById('word_modal').showModal();
+
+    
+};
+
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
@@ -36,7 +71,7 @@ const displayLevelWord = (words) => {
     }
     
     words.forEach(word => {
-        console.log(word);
+        // console.log(word);
         const card = document.createElement('div');
         card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
@@ -45,7 +80,7 @@ const displayLevelWord = (words) => {
 
             <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'Pronunciation পাওয়া যায়নি'}"</div>
             <div class="flex justify-between items-center">
-                <button onclick = 'my_modal_5.showModal()' class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick = 'loadWordDetail(${word.id})' class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>`;
@@ -73,4 +108,4 @@ const displayLesson = lessons => {
     
 }
 
-loadLessons();
+loadLessons();  
